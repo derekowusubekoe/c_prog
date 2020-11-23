0818e832735496c4cb4880596c1172381e4cc82e
Derek Owusu Bekoe <dowusubekoe@outlook.com>
2020-11-23 17:50:00 -0500

    completed the retirement_age task

--- codestepbystep/retirement_age/retirement.c
+++ codestepbystep/retirement_age/retirement.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+void retirement_age();
+int get_number_of_years_left(int assumed_retired_age, int current_age);
 int main()
 {
-    int usersAge;
-    int setRetireAge = 65;
-    int retirement_age;
-    printf("How old are you? ");
-    scanf("%d", &usersAge);
+	retirement_age();
+    return 0;
+}
 
-    printf("You have %d years left until retirement.\n", (retirement_age = setRetireAge - usersAge));
- 
+void retirement_age()
+{
+	int final_retirement_age = 65; // int t = 65;
+	int current_age; // int k;
+
+	printf("How old are you? ");
+	scanf("%d", &current_age);
+
+	int number_of_years_left = get_number_of_years_left(final_retirement_age, current_age);
+	printf("You have %d years left until retirement.\n", number_of_years_left);
+}
+
+int get_number_of_years_left(int assumed_retired_age, int current_age)
+{
+	return ( assumed_retired_age - current_age );
 }
