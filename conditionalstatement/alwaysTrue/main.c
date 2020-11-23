2469ffa26c37cdb632ad5bb151a73667e1bb7da0
Derek Owusu Bekoe <dowusubekoe@outlook.com>
2020-11-23 17:43:42 -0500

    added a conditional statement code

---
+++ conditionalstatement/alwaysTrue/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    a = 5;
+
+    if(a == -3)
+	{
+		printf("%d equals to %d\n",a,-3);
+	}
+	else
+	{
+		printf("%d is not equal to %d\n",a, -3);
+	}
+    return 0;
+}
